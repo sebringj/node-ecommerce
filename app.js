678c9f63b38ed65e537f643cd503c87d5c40717d
Jason Sebring <mail@jasonsebring.com>
2013-01-21 00:08:18 -0800

    6

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var express = require('express'),
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 5000);
+  app.set('port', process.env.PORT || 1337);
   app.engine('ejs', engine);
   app.set('view engine', 'ejs');
   app.set('views', __dirname + '/views');
